5ca892c87b7df8195494ff3fedfa80ab003df5e6
Romeo Miscio <romeomiscio2005@gmail.com>
2024-08-22 00:27:48 +0200

    Spostato l'evento del mouse da "Cliccato" a "Premuto".

--- MouseInput.java
+++ MouseInput.java
@@ -11,19 +11,6 @@ public class MouseInput implements MouseListener {
         // TODO Auto-generated method stub
         // Prendere le coordinate del mouse, aggiungere quelle coord come cibo
 
-        int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
-        int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
-
-        GamePanel.food.add(
-                new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2, y - Game.getyLoc() - GamePanel.antRadius / 2,
-                        false));
-
-        /*
-         * System.out.println("x mouse: " + x + ", y mouse: " + y + "\nfixed x: " + (x -
-         * Game.getxLoc()) + ", fixed y: "
-         * + (y - Game.getyLoc()) + "\nx loc: " + Game.getxLoc() + "\ny loc: " +
-         * Game.getyLoc() + "\n");
-         */
     }
 
     @Override
@@ -42,6 +29,19 @@ public class MouseInput implements MouseListener {
     public void mousePressed(MouseEvent e) {
         // TODO Auto-generated method stub
 
+        int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
+        int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
+
+        GamePanel.food.add(
+                new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2, y - Game.getyLoc() - GamePanel.antRadius / 2,
+                        false));
+
+        /*
+         * System.out.println("x mouse: " + x + ", y mouse: " + y + "\nfixed x: " + (x -
+         * Game.getxLoc()) + ", fixed y: "
+         * + (y - Game.getyLoc()) + "\nx loc: " + Game.getxLoc() + "\ny loc: " +
+         * Game.getyLoc() + "\n");
+         */
     }
 
     @Override
