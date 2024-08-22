2789ce5b4b105d13d807479808e4e73431237a46
Romeo Miscio <romeomiscio2005@gmail.com>
2024-08-22 22:21:17 +0200

    "mousePressed" considera solo il tasto sinistro per spawnare il cibo, mentre "mouseDragged" qualsiasi.

--- MouseInput.java
+++ MouseInput.java
@@ -6,7 +6,49 @@ import javax.swing.event.MouseInputListener;
 
 import java.awt.MouseInfo;
 
+/* 
+ * Codici dei tasti del mouse:
+ * 0 - Nessun tasto premuto
+ * 1 - Tasto sinistro
+ * 2 - Tasto rotella
+ * 3 - Tasto destro
+ */
+
 public class MouseInput implements MouseInputListener {
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+        // ? Considero il tasto sinitro
+        if (e.getButton() == 1) {
+            int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
+            int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
+
+            GamePanel.food.add(new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2,
+                    y - Game.getyLoc() - GamePanel.antRadius / 2, false));
+        }
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+        int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
+        int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
+
+        GamePanel.food.add(
+                new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2,
+                        y - Game.getyLoc() - GamePanel.antRadius / 2,
+                        false));
+
+        /*
+         * System.out.println("x mouse: " + x + ", y mouse: " + y + "\nfixed x: " + (x -
+         * Game.getxLoc()) + ", fixed y: "
+         * + (y - Game.getyLoc()) + "\nx loc: " + Game.getxLoc() + "\ny loc: " +
+         * Game.getyLoc() + "\n");
+         */
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -27,40 +69,9 @@ public class MouseInput implements MouseInputListener {
     }
 
     @Override
-    public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-        int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
-        int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
-
-        GamePanel.food.add(
-                new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2, y - Game.getyLoc() - GamePanel.antRadius / 2,
-                        false));
-    }
-
-    @Override
     public void mouseReleased(MouseEvent e) {
         // TODO Auto-generated method stub
 
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e) {
-        // TODO Auto-generated method stub
-
-        int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
-        int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
-
-        GamePanel.food.add(
-                new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2, y - Game.getyLoc() - GamePanel.antRadius / 2,
-                        false));
-
-        /*
-         * System.out.println("x mouse: " + x + ", y mouse: " + y + "\nfixed x: " + (x -
-         * Game.getxLoc()) + ", fixed y: "
-         * + (y - Game.getyLoc()) + "\nx loc: " + Game.getxLoc() + "\ny loc: " +
-         * Game.getyLoc() + "\n");
-         */
     }
 
     @Override
