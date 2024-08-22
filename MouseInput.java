34fd06baac29a5514b48fa2e38df3dcb47f5c122
Romeo Miscio <romeomiscio2005@gmail.com>
2024-08-22 20:42:01 +0200

    Aggiunto l'evento "mouseDragged" e "mouseMoved".

--- MouseInput.java
+++ MouseInput.java
@@ -1,17 +1,16 @@
 package Formiche;
 
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+
+import javax.swing.event.MouseInputListener;
+
 import java.awt.MouseInfo;
 
-public class MouseInput implements MouseListener, MouseWheelListener {
+public class MouseInput implements MouseInputListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
         // TODO Auto-generated method stub
-        // Prendere le coordinate del mouse, aggiungere quelle coord come cibo
 
     }
 
@@ -31,17 +30,12 @@ public class MouseInput implements MouseListener, MouseWheelListener {
     public void mousePressed(MouseEvent e) {
         // TODO Auto-generated method stub
 
-        int mouseX = (int) MouseInfo.getPointerInfo().getLocation().getX() - Game.getxLoc() - GamePanel.antRadius / 2;
-        int mouseY = (int) MouseInfo.getPointerInfo().getLocation().getY() - Game.getyLoc() - GamePanel.antRadius / 2;
+        int x = (int) MouseInfo.getPointerInfo().getLocation().getX();
+        int y = (int) MouseInfo.getPointerInfo().getLocation().getY();
 
-        GamePanel.food.add(new Punto(mouseX, mouseY, false));
-
-        /*
-         * System.out.println("x mouse: " + x + ", y mouse: " + y + "\nfixed x: " + (x -
-         * Game.getxLoc()) + ", fixed y: "
-         * + (y - Game.getyLoc()) + "\nx loc: " + Game.getxLoc() + "\ny loc: " +
-         * Game.getyLoc() + "\n");
-         */
+        GamePanel.food.add(
+                new Punto(x - Game.getxLoc() - GamePanel.antRadius / 2, y - Game.getyLoc() - GamePanel.antRadius / 2,
+                        false));
     }
 
     @Override
@@ -51,9 +45,16 @@ public class MouseInput implements MouseListener, MouseWheelListener {
     }
 
     @Override
-    public void mouseWheelMoved(MouseWheelEvent e) {
+    public void mouseDragged(MouseEvent e) {
         // TODO Auto-generated method stub
-        // ! Non ho la minima idea di come funzioni sto coso quindi mi devo guardare un
-        // ! tutorial
+        System.out.println("Mouse dragged");
+
     }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
 }
