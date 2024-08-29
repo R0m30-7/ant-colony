01f9ef072be2944fc61917df5dc91b556b0beea8
Romeo Miscio <romeomiscio2005@gmail.com>
2024-08-30 00:19:00 +0200

    Aggiungo il menu al JFrame.

--- GameWindow.java
+++ GameWindow.java
@@ -6,12 +6,17 @@ import javax.swing.JFrame;
 
 public class GameWindow {
     private static JFrame jFrame;
+    protected Menu menu;
 
     public GameWindow(GamePanel gamePanel) {
-        jFrame = new JFrame();
+        jFrame = new JFrame("Ant Colony");
 
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quando chiudo la finestra interrompe il programma
+        //? Aggiunto il menu al JFrame
+        menu = new Menu();
+        //? Aggiungo il gamePanel al JFrame
         jFrame.add(gamePanel);
+        
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quando chiudo la finestra interrompe il programma
         jFrame.setResizable(true);
 
         jFrame.pack();
