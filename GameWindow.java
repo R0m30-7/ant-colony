f278a8c9fc837c60e48997bc89e93ab8e9100f90
Romeo Miscio <romeomiscio2005@gmail.com>
2024-09-01 18:27:15 +0200

    Prima prova di implementazione del menu, aggiunto due void per mostrare e nascondere il menu.

--- GameWindow.java
+++ GameWindow.java
@@ -1,21 +1,22 @@
 package Formiche;
 
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
 public class GameWindow {
     private static JFrame jFrame;
-    protected Menu menu;
+    protected static Menu menu = new Menu();
 
     public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame("Ant Colony");
 
-        //? Aggiunto il menu al JFrame
-        menu = new Menu();
-        //? Aggiungo il gamePanel al JFrame
+        // Aggiungo il gamePanel al JFrame
         jFrame.add(gamePanel);
-        
+
+        // Aggiungo il menu al JFrame, ma lo rendo invisibile
+        jFrame.add(menu);
+        menu.setVisible(false);
+
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quando chiudo la finestra interrompe il programma
         jFrame.setResizable(true);
 
@@ -23,7 +24,19 @@ public class GameWindow {
         jFrame.setVisible(true);
     }
 
-    public static Dimension getjFrameSize(){
+    public static void showMenu() {
+        menu.setVisible(true);
+        jFrame.revalidate();
+        jFrame.repaint();
+    }
+
+    public static void hideMenu() {
+        menu.setVisible(false);
+        jFrame.revalidate();
+        jFrame.repaint();
+    }
+
+    public static Dimension getjFrameSize() {
         return jFrame.getSize();
     }
 }
