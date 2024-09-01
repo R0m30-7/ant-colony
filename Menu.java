6ecadfb429880328ce7c1a024a7582964e620c0b
Romeo Miscio <romeomiscio2005@gmail.com>
2024-09-01 18:29:36 +0200

    Disegno un pulsante.

---
+++ Menu.java
@@ -0,0 +1,11 @@
+package Formiche;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Menu extends JPanel{
+    public Menu() {
+        JButton button = new JButton("Cliccami!");
+        add(button);
+    }
+}
