c7a474277dfbd7b9e7a850b52217b26dca87bbe4
Romeo <romeomiscio2005@gmail.com>
2025-02-21 11:43:13 +0100

    Modificato il path delle immagini delle formiche

--- GamePanel.java
+++ GamePanel.java
@@ -349,14 +349,14 @@ public class GamePanel extends JPanel {
     private void LoadImages(Graphics g){
         //! Quando compilo in un file jar, la directory delle immagini dovrà contenere solo il nome e l'estensione, come nell'esempio: pathname:"antWithFood.png"
         //System.out.println(System.getProperty("user.dir"));
-        File file = new File("Java\\Formiche\\antWithFood.png");
+        File file = new File("antWithFood.png");
         
         try {
             antWithFood = ImageIO.read(file);
         } catch (IOException e) {
             //System.out.println("L'immagine della formica con il cibo non esiste");
         }
-        file = new File("Java\\Formiche\\antWithoutFood.png");
+        file = new File("antWithoutFood.png");
         try {
             antWithOutFood = ImageIO.read(file);
         } catch (IOException e) {
